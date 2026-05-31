eb66e0a0e3579a6a745bf42bc6980fb60213223c
METHEW <yotoqxona.com@gmail.com>
2026-05-31 10:54:27 +0900

    fix: router admin name according to project  standarts

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,14 +2,16 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controllers";
 
+/* Restaurant */
 routerAdmin.get('/', restaurantController.goHome);
-
 routerAdmin
     .get('/login', restaurantController.getLogin)
     .post('/login', restaurantController.processLogin);
-
 routerAdmin
     .get('/signup', restaurantController.getSignup)
     .post('/signup', restaurantController.processSignup);
+
+/* Producty */
+/* User */
 
 export default routerAdmin;
