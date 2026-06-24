5f723a93f1363ecc92146d8acb1010c9c1a3eec6
METHEW <yotoqxona.com@gmail.com>
2026-06-24 17:51:39 +0900

    feat: frontend chosen update logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,30 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+    $(".member-status").on("change", function (e) {
+        const id = e.target.id;
+        console.log("id:", id);
+
+        const memberStatus = $(`#${id}.member-status`).val();
+        console.log("memberStatus:", memberStatus)
+
+        axios
+            .post("/admin/user/edit", {
+                _id: id,
+                memberStatus: memberStatus,
+            }).then((response) => {
+                console.log("response:", response);
+                const result = response.data;
+                console.log("result:", result);
+
+                if (result.data) {
+                    console.log(" user updated");
+                    $(".member-status").blur();
+                } else alert("user update failed!");
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("user update failed!");
+            });
+    })
+})
