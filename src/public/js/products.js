b174be599d1f0ab65efa892826679dfe91e02feb
METHEW <yotoqxona.com@gmail.com>
2026-06-24 15:37:50 +0900

    feat: update chusen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -23,6 +23,27 @@ $("#cancel-btn").on("click", () => {
     $("#process-btn").css("display", "flex");
 });
 
+$(".new-product-status").on("change", async function (e) {
+    const id = e.target.id;
+    const productStatus = $(`#${id}.new-product-status`).val();
+    console.log("id:", id);
+    console.log("productStatus:", productStatus);
+
+    try {
+        const response = await axios.post(`/admin/product/${id}`, { productStatus: productStatus });
+        console.log("response:", response)
+        const result = response.data;
+        if (result.data) {
+            console.log("product updated")
+            $(".new-product-status").blur();
+        } else alert(" product update failed !")
+    } catch (err) {
+        console.log(err);
+        alert(" product update failed !");
+    }
+});
+
+
 function validateForm() {
     const productName = $(".product-name").val();
     const productPrice = $(".product-price").val();
